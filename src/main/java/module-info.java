726c8d0e67144f5b26f121ad3b0fcce8ec769bb1
dhyaanesh123 <dhyaanesh03@gmail.com>
2026-07-06 22:20:33 +0800

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.ap_assignment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.ap_assignment to javafx.fxml;
-    exports org.example.ap_assignment;
-}

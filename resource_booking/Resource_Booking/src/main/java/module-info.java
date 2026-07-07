8c2ee9bb40317ff252cc558bdbe42cbe6cb31d6d
dhyaanesh123 <dhyaanesh03@gmail.com>
2026-07-07 19:18:54 +0800

    Delete resource_booking/Resource_Booking/src/main directory

--- resource_booking/Resource_Booking/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.resource_booking {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.resource_booking to javafx.fxml;
-    exports org.example.resource_booking;
-}
--- resource_booking/Resource_Booking/src/main/java/org/example/resource_booking/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.resource_booking;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
